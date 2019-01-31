6d10732d9c155a7dfb21af3853f125f3ecdd6b3f
Stas Dulman <stas.dulman@samanage.com>
2019-01-31 11:04:38 +0200

    remove hot loader from babel prod

--- examples/client/scripts/main.js
+++ examples/client/scripts/main.js
@@ -2,7 +2,7 @@ import path from 'path';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { util } from 'react-lightning-design-system';
-import { hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader';
 import Root from './components/Root';
 
 util.setAssetRoot(path.join(location.pathname, 'assets'));
